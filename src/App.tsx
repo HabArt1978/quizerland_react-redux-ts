827a74b6bd684bd8a2ce1293ef77eefe45f8bc05
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-10-21 13:00:35 +0300

    Применение prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,74 @@
-import { Routes, Route } from "react-router-dom"
-import { useEffect } from "react"
-import { useAppDispatch } from "./store/hooks"
+import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useAppDispatch } from './store/hooks'
 
-import api from "./api"
-import { setUser } from "./store/auth/actions"
-import MenuToggle from "./components/Navigation/MenuToggle"
+import api from './api'
+import { setUser } from './store/auth/actions'
+import MenuToggle from './components/Navigation/MenuToggle'
 
-import QuizesPage from "./pages/quizes/index"
-import ActiveQuizPage from "./pages/quizes/_id"
-import CreateQuizPage from "./pages/quizes/create"
-import AuthPage from "./pages/auth"
-import RedirectPage from "./pages/redirect"
-import NotFoundPage from "./pages/404/notFound"
-import RegistrationPage from "./pages/auth/registration"
-import QuizesPassed from "./pages/quizes/quizesPassed/QuizesPassed"
+import QuizesPage from './pages/quizes/index'
+import ActiveQuizPage from './pages/quizes/_id'
+import CreateQuizPage from './pages/quizes/create'
+import AuthPage from './pages/auth'
+import RedirectPage from './pages/redirect'
+import NotFoundPage from './pages/404/notFound'
+import RegistrationPage from './pages/auth/registration'
+import QuizesPassed from './pages/quizes/quizesPassed/QuizesPassed'
 
-import "./index.css"
+import './index.css'
 
 function App() {
-  const dispatch = useAppDispatch()
+	const dispatch = useAppDispatch()
 
-  useEffect(() => {
-    // Запрос выполняется дважды в дев окружении, т.к. включен React.StrictMode
-    api.auth
-      .user()
-      .then(user => dispatch(setUser(user)))
-      .catch(error =>
-        console.error(`Пользователь не зарегистрирован! / ${error}`),
-      )
-  }, [dispatch])
+	useEffect(() => {
+		// Запрос выполняется дважды в дев окружении, т.к. включен React.StrictMode
+		api.auth
+			.user()
+			.then((user) => dispatch(setUser(user)))
+			.catch((error) =>
+				console.error(`Пользователь не зарегистрирован! / ${error}`),
+			)
+	}, [dispatch])
 
-  return (
-    <>
-      <MenuToggle />
-      <Routes>
-        <Route
-          path="/quizes/:id"
-          element={<ActiveQuizPage />}
-        />
-        <Route
-          path="/quizes"
-          element={<QuizesPage />}
-        />
-        <Route
-          path="/auth"
-          element={<AuthPage />}
-        />
-        <Route
-          path="/registration"
-          element={<RegistrationPage />}
-        />
-        <Route
-          path="/create-quiz"
-          element={<CreateQuizPage />}
-        />
-        <Route
-          path="/quizes-passed"
-          element={<QuizesPassed />}
-        />
-        <Route
-          path="/"
-          element={<RedirectPage />}
-        />
-        <Route
-          path="*"
-          element={<NotFoundPage />}
-        />
-      </Routes>
-    </>
-  )
+	return (
+		<>
+			<MenuToggle />
+			<Routes>
+				<Route
+					path='/quizes/:id'
+					element={<ActiveQuizPage />}
+				/>
+				<Route
+					path='/quizes'
+					element={<QuizesPage />}
+				/>
+				<Route
+					path='/auth'
+					element={<AuthPage />}
+				/>
+				<Route
+					path='/registration'
+					element={<RegistrationPage />}
+				/>
+				<Route
+					path='/create-quiz'
+					element={<CreateQuizPage />}
+				/>
+				<Route
+					path='/quizes-passed'
+					element={<QuizesPassed />}
+				/>
+				<Route
+					path='/'
+					element={<RedirectPage />}
+				/>
+				<Route
+					path='*'
+					element={<NotFoundPage />}
+				/>
+			</Routes>
+		</>
+	)
 }
 
 export default App
--- src/components/Navigation/Drawer/Drawer.tsx
+++ src/components/Navigation/Drawer/Drawer.tsx
@@ -1,45 +1,45 @@
-import { useAppDispatch } from "../../../store/hooks"
-import { useAppSelector } from "../../../store/hooks"
+import { useAppDispatch } from '../../../store/hooks'
+import { useAppSelector } from '../../../store/hooks'
 
-import { menuToggle } from "../../../store/navigation/actions"
+import { menuToggle } from '../../../store/navigation/actions'
 
-import LinksList from "./LinksList"
+import LinksList from './LinksList'
 
-import IconButton from "@mui/material/IconButton"
-import Box from "@mui/material/Box"
-import Drawer from "@mui/material/Drawer"
-import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew"
+import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 
 const MenuDrawer = () => {
-  const { isOpen } = useAppSelector(({ navigationState }) => navigationState)
-  const dispatch = useAppDispatch()
+	const { isOpen } = useAppSelector(({ navigationState }) => navigationState)
+	const dispatch = useAppDispatch()
 
-  return (
-    <>
-      <Drawer
-        anchor={"left"}
-        open={isOpen}
-        onClose={() => dispatch(menuToggle())}
-      >
-        <IconButton
-          style={{
-            borderRadius: "50%",
-            margin: "0.3rem 0.3rem 0rem auto",
-            color: "mediumpurple",
-          }}
-          onClick={() => dispatch(menuToggle())}
-        >
-          <ArrowBackIosNewIcon />
-        </IconButton>
-        <Box
-          sx={{ width: 300 }}
-          role="presentation"
-        >
-          <LinksList />
-        </Box>
-      </Drawer>
-    </>
-  )
+	return (
+		<>
+			<Drawer
+				anchor={'left'}
+				open={isOpen}
+				onClose={() => dispatch(menuToggle())}
+			>
+				<IconButton
+					style={{
+						borderRadius: '50%',
+						margin: '0.3rem 0.3rem 0rem auto',
+						color: 'mediumpurple',
+					}}
+					onClick={() => dispatch(menuToggle())}
+				>
+					<ArrowBackIosNewIcon />
+				</IconButton>
+				<Box
+					sx={{ width: 300 }}
+					role='presentation'
+				>
+					<LinksList />
+				</Box>
+			</Drawer>
+		</>
+	)
 }
 
 export default MenuDrawer
